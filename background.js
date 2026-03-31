95c3acc9c3ac64264a7309377a7e172c291d28c6
Cook I.T! <55697931+Cook-I-T@users.noreply.github.com>
2026-03-31 15:58:56 +0200

    Cleanup & rename
    Removed the console.log() used for debugging and expanded comments.
    Renamed addon to "MaxTabsLimit-XT" to differentiate this fork.
    Also add *.xpi to .gitignore just in case

--- background.js
+++ background.js
@@ -9,82 +9,70 @@ var managArr;
 var localArr;
 var isLoading = false;
 
-//set default and managed values to local storage, do not overwrite unless locked = true in managed storage.
+//add default and managed values to local storage & then to variable
+//Do not overwrite local storage unless locked = true in managed storage.
 async function loadStorageValues() {
   //Make double-sure it does not cause infinite recursions!!
-  browser.storage.onChanged.removeListener(configUpdated);
-  console.log("loadStorageValues: locking now")
   isLoading = true;
+  browser.storage.onChanged.removeListener(configUpdated);
   //always reset locked to prevent a case where managed storage is removed, leaving the settings unchangeable
-  await browser.storage.local.remove("locked")
+  await browser.storage.local.remove("locked");
   //get the keys of the local storage as an array, useful for not overwriting things
-  await browser.storage.local.get().then(data => {localArr = Object.keys(data);})
-  console.log("loadStorageValues: localArr:")
-  console.log(localArr)
+  await browser.storage.local.get().then(data => {localArr = Object.keys(data);});
   //get the keys of the managed storage as an array, if there is an error just save the error which is NOT an array
-  await browser.storage.managed.get().then(data => {managArr = Object.keys(data);}, err => {managArr = err})
-  console.log("loadStorageValues: managArr")
-  console.log(managArr)
+  await browser.storage.managed.get().then(data => {managArr = Object.keys(data);}, err => {managArr = err});
   /*
   If browser.storage.managed.get() does not throw an error, it will be an array.
   We prioritize the managed settings by adding, or if locked overwriting, first and only then adding the default settings for missing entries after everything else.
   */
   if (Array.isArray(managArr)) {
     let result = await browser.storage.managed.get("locked");
-    //if locked overwrite instead of adding
+    //if locked overwrite instead of add
     if (result.locked === true) {
-      //Can't use .forEach as that causes some async shenanigans as await does not work on it
+      //Can't use .forEach as that causes some async shenanigans because await does not work on it
       for (const key of managArr) {
         await writeManagedToLocal(key);
-      } //await managArr.forEach(key=>{writeManagedToLocal(key)});
+      }
     } else {
-      //only set variables not already set
+      //filter out already existing keys
       let toRemove = new Set(localArr);
       let toSet = managArr.filter( x => !toRemove.has(x) );
-      console.log("loadStorageValues: toSet:");
-      console.log(toSet);
+      //only set variables not already set
       for (const key of toSet) {
         await writeManagedToLocal(key);
-      } //await toSet.forEach(key=>{writeManagedToLocal(key)});
+      }
     }
-  } else {
-    console.log("loadStorageValues: managArr is not Array, assuming managed Storage is not in use.")
   }
-  //lastly always add the default values to ensure nothing stays empty
+  //lastly always add the default values to ensure nothing required stays empty
   //Refresh localArr
   await browser.storage.local.get().then(data => {localArr = Object.keys(data);})
+  //filter out already existing keys
   let toRemove = new Set(localArr);
   let toSet = Object.keys(defaultSettings).filter( x => !toRemove.has(x) );
-  console.log("loadStorageValues: toSetDefault:");
-  console.log(toSet);
   for (const key of toSet) {
     await browser.storage.local.set({[key]: defaultSettings[key]});
-  } //await toSet.forEach(key=>{browser.storage.local.set({[key]: defaultSettings[key]})});
+  }
 
-  //Copy local storage to variable
+  //Copy entire local storage to variable for easy non-async access
   await browser.storage.local.get().then(data => {localArr = Object.keys(data);});
   for (const key of localArr) {
     await writeLocalToVariable(key);
-  } //await localArr.forEach(key=>{writeLocalToVariable(key)});
+  }
 
   //If config gets updated in settings page reload everything
   browser.storage.onChanged.addListener(configUpdated);
-  console.log("loadStorageValues: unlocking");
   isLoading = false;
 }
 
 async function writeManagedToLocal(key) {
   let manval;
   await browser.storage.managed.get(key).then(data => {manval = data[key];});
-  console.log("writeManagedToLocal: Setting " + key.toString() + " to " + manval);
   await browser.storage.local.set({[key]: manval});
-  //console.log(key.toString() + " is set to: " + manval);
 }
 
 async function writeLocalToVariable(key) {
   let locval;
   await browser.storage.local.get(key).then(data => {locval = data[key];});
-  console.log("writeLocalToVariable: Variabelizing " + key.toString() + " with " + locval);
   currentSettings[key] = locval;
 }
 
@@ -94,13 +82,10 @@ Update the badge. Including text and color.
 Notify user, when too many tabs were opened.
 */
 function updateCount(tabId, isOnRemoved) {
-  console.log("updateCount called")
   browser.tabs.query({})
   .then((tabs) => {
     let length = tabs.length;
-    console.log("length is: " + length)
     if (tabId == undefined) {
-      console.log("tabId == undefined")
       updateBadge(length);
       return;
     }
@@ -137,24 +122,20 @@ function updateCount(tabId, isOnRemoved) {
 Display tab count on badge and switch color depending on how close user is to maxTabs limit.
 */
 function updateBadge(length) {
-  console.log("updateBadge called, length: " + length.toString() + "; maxTabs: " + currentSettings["maxTabs"].toString());
   switch(currentSettings["badgeFormat"]) {
     case "openTabs":
-      console.log("case " + currentSettings["badgeFormat"] + " : " +  length.toString());
       browser.browserAction.setBadgeText({text: length.toString()});
       break;
     case "remainingTabs":
-      console.log("case " + currentSettings["badgeFormat"] + " : " +  (currentSettings["maxTabs"] - length).toString());
       browser.browserAction.setBadgeText({text: (currentSettings["maxTabs"] - length).toString()});
       break;
     case "openTabsMax":
-      console.log("case " + currentSettings["badgeFormat"] + " : " +  length.toString() + "/" + currentSettings["maxTabs"].toString());
       browser.browserAction.setBadgeText({text: length.toString() + "/" + currentSettings["maxTabs"].toString()});
       break;
     case "remainingTabsMax":
-      console.log("case " + currentSettings["badgeFormat"] + " : " +  (currentSettings["maxTabs"] - length).toString() + "/" + currentSettings["maxTabs"].toString());
       browser.browserAction.setBadgeText({text: (currentSettings["maxTabs"] - length).toString() + "/" + currentSettings["maxTabs"].toString()});
       break;
+    //in case managed storage or something else creates an invalid setting, fall back to openTabs
     default:
       browser.browserAction.setBadgeText({text: length.toString()});
   }
@@ -172,20 +153,17 @@ function updateBadge(length) {
 Retrieve the values of the updated config from storage and update the UI accordingly.
 */
 async function configUpdated() {
-  console.log("configUpdated: Update called!");
   if (!isLoading) {
     await loadStorageValues();
   } else {
-    console.log("configUpdated: configUpdated got called while isLoading is true!");
+    console.error("configUpdated got called while isLoading is true! This should not happen!");
     return;
   }
-  console.log("unlock");
   browser.tabs.query({})
   .then((tabs) => {
     let length = tabs.length;
     updateBadge(length);
   });
-  console.log("configUpdated: configUpdated done")
 }
 
 async function main() {
@@ -202,7 +180,6 @@ async function main() {
   });
 }
 
-console.log("Calling main()")
-//Have to do it like this to be able to use await.
+//Have to do it like this to be able to use await on loadStorageValues
 main()
-//Do not put stuff after main(), since it will be executed before main since it's async.'
+//Do not put stuff after main(), since it will be immediatly executed before main since it's async.
--- options.js
+++ options.js
@@ -4,14 +4,12 @@ var currentSettings = {};
 //Radio buttons sadly need special treatment :(
 var valueFormElementIDs = ['maxTabs', 'errorTitle', 'errorContent'];
 var allFormElementIDs = ['maxTabs', 'openTabs', 'remainingTabs', 'openTabsMax', 'remainingTabsMax', 'errorTitle', 'errorContent', 'submit'];
+
 /**
  * Update the UI: set the value of the maxTabs textbox.
  */
 async function updateUI() {
-  console.log("updateUI")
-  //check if settings are locked and/or overwritten by managed storage
-
-  //Copy local storage to variable, make sure it's done by using for loop with await
+  //Copy local storage to variable, make sure it's done by using for loop with await as .forEach doesn't (a)wait
   await browser.storage.local.get().then(data => {localArr = Object.keys(data);});
   for (const key of localArr) {
     await writeLocalToVariable(key);
@@ -22,6 +20,7 @@ async function updateUI() {
   //Select the correct radio button
   document.getElementById(currentSettings["badgeFormat"]).checked = true;
 
+  //disable all form elements if locked is true
   if (currentSettings["locked"] === true) {
     allFormElementIDs.forEach(key=>{document.getElementById(key).setAttribute('disabled', '')});
     if (currentSettings["lockText"] != "" && typeof currentSettings["lockText"] == "string") {
@@ -34,26 +33,26 @@ async function updateUI() {
 async function writeLocalToVariable(key) {
   let locval;
   await browser.storage.local.get(key).then(data => {locval = data[key];});
-  console.log("writeLocalToVariable: Variabelizing " + key.toString() + " with " + locval);
   currentSettings[key] = locval;
 }
 
 
+
 /**
- * Save settings to storage.
+ * Save settings to local storage.
  */
 function saveOptions() {
+  //ensure nothing happens even if the form is somehow submitted while locked
   if (currentSettings["locked"] === true) {
     return;
   }
+  //concatenate into 1 object so storage.onChanged listener only fires once
   let toSave = {};
   //radio buttons need special treatment
   toSave["badgeFormat"] = document.querySelector('input[name="badgeFormat"]:checked').value;
   for (const id of valueFormElementIDs) {
     toSave[id] = document.getElementById(id).value;
   }
-  console.log("saveOptions: toSave variable is:");
-  console.log(toSave);
   browser.storage.local.set(toSave).then(savedSuccessfully, onError);
 }
 
@@ -71,7 +70,7 @@ function savedSuccessfully() {
 }
 
 /*
-Generic error logger.
+Generic error logger with notification.
 */
 function onError(e) {
   console.error(e);
@@ -86,7 +85,7 @@ function onError(e) {
 /**
  * Update the UI when the page loads.
  */
-console.log("addEventListeners")
 document.addEventListener('DOMContentLoaded', updateUI);
-// document.querySelector("form").addEventListener("submit", saveOptions);
+//without proper .preventDefault() the page gets reloaded after browser.storage.local.set in saveOptions()
+//While it "works" (settings get saved) it flashes while reloading and stops before the success notification & log entry.
 document.querySelector('form').addEventListener('submit', (event) => {event.preventDefault(); saveOptions()});
